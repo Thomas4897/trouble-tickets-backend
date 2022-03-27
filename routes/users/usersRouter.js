2ed0d7155427b5700ad01e8a38d745aa13998c5e
Thomas Maynard <90271276+Thomas4897@users.noreply.github.com>
2022-03-27 17:14:42 +0800

    Updating UserRouter

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("Hello from usersRouter");
+  // res.send("Hello from usersRouter");
+  res.send({
+    title: "Hello form userRouter",
+  });
 });
 
 module.exports = router;
